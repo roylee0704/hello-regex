f3a622b071e415ad3b5672607f0e97f800ca7a3f
roylee0704 <roylee0704@gmail.com>
2021-11-06 13:20:03 +0800

    update

--- rhb-account-selector.js
+++ rhb-account-selector.js
@@ -5,4 +5,7 @@ const s = fs.readFileSync('rhb-account-selector.in', 'utf-8').trim();
 
 const re = new RegExp(/\s+(\d[\d|-]+)\s+/);
 
-console.log('account id:', s.match(re)[0]);
+const [a, accountId] = s.match(re);
+
+
+console.log(a, accountId);
